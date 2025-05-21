59084e80d374694398c70f5e6b247e073e48b616
JunPeter <peter950306@gmail.com>
2025-05-22 01:54:49 +0900

    unity 123
    unity 123

--- Assets/asd.cs
+++ Assets/asd.cs
@@ -1,12 +1,11 @@
 using UnityEngine;
 
-public class Study : MonoBehaviour
+public class asd : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("start");
-        Debug.Log("start");
+        
     }
 
     // Update is called once per frame
